9291a46df53548af7c309d75c12fd81aef098370
rkislov <roman@kislovs.ru>
2020-03-11 11:32:34 +0500

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'admin', 'namespace'=>'Admin', 'middlewear'=>['auth']], function () {
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
